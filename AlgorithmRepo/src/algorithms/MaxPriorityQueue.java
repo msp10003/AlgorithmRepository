73b033792dd8e02e358223e050c81a3bd53ae049
msp10003 <matthew.s.pierce@uconn.edu>
2017-02-28 17:38:29 -0500

    Implemented priority queue methods and added array resize util method

--- AlgorithmRepo/src/algorithms/MaxPriorityQueue.java
+++ AlgorithmRepo/src/algorithms/MaxPriorityQueue.java
@@ -1,9 +1,87 @@
 package algorithms;
 
-public class MaxPriorityQueue {
+import utils.Utils;
 
+public class MaxPriorityQueue<Key extends Comparable<Key>>{
+	private Key[] pq;
+	int N;
+	
 	public MaxPriorityQueue(){
+		N = 0;
+		pq = (Key[]) new Comparable[2];
+	}
+	
+	public MaxPriorityQueue(int max){
 		
 	}
 	
+	public void insert(Key v){
+		N++;
+		Utils.resizeArray(pq, N+1);
+		pq[N] = v;
+		swim(N);
+	}
+	
+	public Key max(){
+		return pq[1];
+	}
+	
+	public Key delMax(){
+		Key max = pq[1];
+		pq[1]= pq[N];
+		pq[N] = null;
+		sink();
+		Utils.resizeArray(pq, --N);
+		return max;
+	}
+	
+	public boolean isEmpty(){
+		return N == 0;
+	}
+	
+	public int size(){
+		return N;
+	}
+	
+	private void sink(){
+		int k = 1;
+		
+		while(2*k <= N){
+			int j = 2*k;
+			//if j was >= N, it's the last key, so don't look for sibling
+			if (j <N && less(j, j+1)) j++;
+			//if k ain't less than j, just stop
+			if (!less(k, j)) break;
+			exch(k, j);
+			k = j;
+		}
+	}
+	
+	private void swim(int k){
+		while(k > 1 && less(k/2, k)){
+			exch(k/2, k);
+			k = k/2;
+		}
+	}
+	
+	/*alternative way to do this a swim uses recursion*/
+	private void swim_r(int k){
+		//base cases
+		if(k/2 == 1) return;
+		else if (!less(k/2, k)) return;
+		
+		exch(k/2, k);
+		swim(k/2);
+	}
+	
+	
+	private boolean less(int i, int j){
+		return pq[i].compareTo(pq[j]) < 0;
+	}
+	
+	private void exch(int i, int j){
+		Key temp = pq[i];
+		pq[i] = pq[j];
+		pq[j] = temp;
+	}
 }
--- AlgorithmRepo/src/utils/Utils.java
+++ AlgorithmRepo/src/utils/Utils.java
@@ -12,5 +12,20 @@ public class Utils {
 	{
 		return a.compareTo(b) < 0;
 	}
+	
+	public static void resizeArray(Object[] a, int elems){
+		if(elems >= a.length/2){
+			Object[] t = new Object[a.length*2];
+			for(int i = 0; i< elems; i++){
+				t[i] = a[i];
+			}
+		}
+		else if(elems <= a.length/4){
+			Object[] t = new Object[a.length/2];
+			for(int i = 0; i< elems; i++){
+				t[i] = a[i];
+			}
+		}
+	}
 
 }
